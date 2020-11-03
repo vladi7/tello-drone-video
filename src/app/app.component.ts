5261d3a633e44d91e1c98c73c1f6137c2400c7ac
Han0608 <54295227+Han0608@users.noreply.github.com>
2020-11-03 09:52:07 -0600

    Commit improved front-end -Han

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,15 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, ElementRef, ViewChild} from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+  @ViewChild('myDiv') myDiv: ElementRef;
+  timer;
+
+  ngOnInit() {
+    this.timer = setInterval(() => {
+      this.myDiv.nativeElement.innerHTML = new Date();
+    }, 1000);
+  }
   //title = 'DroneVideoRecognition';
    move(direction) {
     console.log(direction);
   }
+
+
 }
 
 
